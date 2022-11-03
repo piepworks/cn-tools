500137889e118ee240f558abfc8248c0ff060c97
Trey Piepmeier <trey@treypiepmeier.com>
2022-11-02 22:48:05 -0400

    Figure out what to do with nested stuff.
    Now to figure out why I need to do…
    
    $preset['name']
    
    …instead of
    
    $preset->name
    
    …like is mentioned in this bit about looping:
    https://laravel.com/docs/9.x/blade#loops

--- app/Http/Controllers/CropfactorController.php
+++ app/Http/Controllers/CropfactorController.php
@@ -9,14 +9,15 @@ class CropfactorController extends Controller
     public function index(Request $request)
     {
         // print_r($this->zippy(3));
-        print_r(config('cf.presets.full.dimensions.width'));
+        // print_r(config('cf.presets.aps-ch.items'));
 
         return view ('cropfactor', [
-            'preset' => $request->query('preset') ?? 'full',
-            'height' => $request->query('height') ?? null,
-            'width' => $request->query('width') ?? null,
-            'focalLength' => $request->query('focal_length') ?? 50,
-            'fStop' => $request->query('f_stop') ?? 1.8,
+            'presets' => config('cf.presets'),
+            // 'preset' => $request->query('preset') ?? 'full',
+            // 'height' => $request->query('height') ?? null,
+            // 'width' => $request->query('width') ?? null,
+            // 'focalLength' => $request->query('focal_length') ?? 50,
+            // 'fStop' => $request->query('f_stop') ?? 1.8,
         ]);
     }
 
--- resources/views/cropfactor.blade.php
+++ resources/views/cropfactor.blade.php
@@ -1,7 +1,20 @@
 <x-layout title="Crop Factor Calculator">
     <h1>Crop Factor Calculator</h1>
 
-    {{ $preset }}
+    <ul>
+    @foreach ($presets as $preset)
+        <li>
+        {{ $preset['name'] }}
+        @if (@isset($preset['items']))
+            <ul>
+            @foreach ($preset['items'] as $item)
+                <li>{{ $item['name'] }}</li>
+            @endforeach
+            </ul>
+        @endif
+        </li>
+    @endforeach
+    </ul>
 
     <form action="">
         <button>Submit</button>
