e83e7b31627ae3e374282b07c6f74c60ca35dc48
Trey Piepmeier <trey@treypiepmeier.com>
2022-11-05 09:24:48 -0400

    Don't blow up if a preset doesn't exist.

--- app/Http/Controllers/CropfactorController.php
+++ app/Http/Controllers/CropfactorController.php
@@ -8,18 +8,23 @@ class CropfactorController extends Controller
 {
     public function index(Request $request)
     {
-        // print_r($this->zippy(3));
+        // dd($this->zippy(3));
         $presets = config('cf.presets');
         $currentPreset['selector'] = $request->query('preset') ?? 'full';
         $pieces = explode('.', $currentPreset['selector']);
 
         if (count($pieces) > 1) {
-            $selector = $pieces[0] . '.presets.' . $pieces[1];
+            $fullSelector = $pieces[0] . '.presets.' . $pieces[1];
         } else {
-            $selector = $pieces[0];
+            $fullSelector = $pieces[0];
         }
 
-        $currentPreset['values'] = data_get($presets, $selector);
+        $currentPreset['values'] = data_get($presets, $fullSelector);
+
+        if ($currentPreset['values'] == null) {
+            abort(404);
+        }
+
         $results['focalLength'] = $request->query('focal_length') ?? 50;
         $results['fStop'] = $request->query('f_stop') ?? 1.8;
         $results['height'] = $request->query('height') ?? $currentPreset['values']['dimensions']['height'];
