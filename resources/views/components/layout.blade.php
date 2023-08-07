9058ded77418ea5fe23de44f5097889d0147bb83
Trey Piepmeier <trey@treypiepmeier.com>
2023-08-07 17:08:24 -0400

    Not needed yet

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>
         @if (@isset($title))
             {{ $title }} /
@@ -14,7 +13,7 @@
     <script src="/htmx-1.9.4.min.js"></script>
     <script src="/idiomorph-ext-0.0.8.min.js"></script>
     <script src="/d3.v7.min.js"></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css'])
 </head>
 
 <body>
